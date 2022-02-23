614509dffa722d3a5857862fbe007ff2083cb765
ecfidler <ethancf1999@gmail.com>
2022-02-23 10:45:50 -0500

    hw 2

--- HW2Q6/HW2Q6_stats.c
+++ HW2Q6/HW2Q6_stats.c
@@ -22,29 +22,79 @@ void *calculate_average(void *param);
 void *calculate_maximum(void *param);
 void *calculate_minimum(void *param);
 
+int argLength;
+
 int main(int argc, char *argv[])
 {
-	/* write your code: allocate memory to hold array of integers, create the threads, wait for the threads to exit, and print out the results */
+	pthread_t tid0;
+	pthread_t tid1;
+	pthread_t tid2;
+	pthread_attr_t attr0;
+	pthread_attr_t attr1;
+	pthread_attr_t attr2;
 
+
+	if (argc <= 2) {
+		printf("Not enough Arguments passed!");
+		return -1;
+	}
+
+	list = malloc((argc-1) * sizeof(int));
+
+	argLength = argc-1;
+
+	for (int i = 0; i < argLength; i++) {
+		list[i] = atoi(argv[i+1]);
+	}
+
+	pthread_attr_init(&attr0);
+	pthread_attr_init(&attr1);
+	pthread_attr_init(&attr2);
+
+	pthread_create(&tid0,&attr0,calculate_average,list);
+	pthread_create(&tid1,&attr1,calculate_maximum,list);
+	pthread_create(&tid2,&attr2,calculate_minimum,list);
+
+	pthread_join(tid0,NULL);
+	pthread_join(tid1,NULL);
+	pthread_join(tid2,NULL);
+
+	printf("average = %f\n",average);
+	printf("maximum = %d\n",maximum);
+	printf("minimum = %d\n",minimum);
 
 	return 0;
 }
 
 void *calculate_average(void *param)
 {
-	/*write your code here*/
-
+	int sum = 0;
+	for (int i = 0; i< argLength; i++) {
+		printf("%d ", list[i]);
+		sum += list[i];
+	}
+	average = (double)sum/(double)argLength;
 }
 
 void *calculate_maximum(void *param)
 {
-	/*write your code here*/
-
+	int max = 0;
+	for (int i = 0; i < argLength; i++) {
+		if (list[i]>max) {
+			max = list[i];
+		}
+	}
+	maximum = max;
 }
 
 void *calculate_minimum(void *param)
 {
-	/*write your code here*/
-
+	int min = list[0];
+	for (int i = 0; i < argLength; i++) {
+		if (list[i]<min) {
+			min = list[i];
+		}
+	}
+	minimum = min;
 }
 
